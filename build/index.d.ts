09fc5d02de0856bae77a9858a8a786efd89fff7e
Abraham Elmahrek <abraham@elmahrek.com>
2018-01-23 17:04:09 -0800

    Add interfaces to list of exports

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,3 +1,5 @@
 import getClient from "./client";
 import ContractDefinitionLoader from "./ContractDefinitionLoader";
 export { getClient, ContractDefinitionLoader };
+export * from "./IContractDefinitions";
+export * from "./IContractDefinition";
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ import getClient from "./client";
 import ContractDefinitionLoader from "./ContractDefinitionLoader";
 
 export { getClient, ContractDefinitionLoader };
+
+export * from "./IContractDefinitions";
+export * from "./IContractDefinition";
